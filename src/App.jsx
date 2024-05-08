da536c17eb53be3de29cdb24ab944e8a80401d86
jeflugo <jefersoneduardoluego@gmail.com>
2024-05-07 22:19:56 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Timer from './components/Timer'
+
+function App() {
+	return (
+		<div className='w-[500px]'>
+			<h1>Quizz app</h1>
+			<div>
+				<Timer />
+			</div>
+		</div>
+	)
+}
+
+export default App
